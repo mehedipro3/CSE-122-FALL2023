9e02e800a52fadb37606e356fae3c6f30ff814dd
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-08-06 00:18:23 +0600

    lab-02.cpp

---
+++ lab/lab-02.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+int  main ()
+{
+    string s;
+    int m;
+    cin >> m;
+    for (int i=0; i<m; i++)
+    {
+        cin >> s ;
+        if(s.length()>10){
+            cout << s[0] << s.length()-2 << s[s.length()-1] << endl;
+        }
+        else {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
+
