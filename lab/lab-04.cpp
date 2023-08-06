43d2f7cd23ab0a0475590b3cd2943e906f16d2b7
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-08-06 11:44:23 +0600

    Create lab-04.cpp

---
+++ lab/lab-04.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std ;
+int  main ()
+{
+    int n,k,i,a[1000],counter=0;
+    cin >> n >> k;
+    for (i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    for (i=0; i<n; i++)
+    {
+        if(a[i] >= a[k-1] && a[i]>0)
+        {
+            counter ++;
+        }
+    }
+
+    cout << counter << endl;
+
+    return 0;
+}
+
