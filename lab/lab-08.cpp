4259ec833b21921f0d8fc6a4c1a41309266f43ca
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-08-20 14:36:30 +0600

    Update lab-08.cpp

--- lab/lab-08.cpp
+++ lab/lab-08.cpp
@@ -4,40 +4,36 @@
 
 #include<iostream>
 #include<string>
-using namespace std ;
-int  main ()
-{
-    string a,b;
-    int i;
-    cin >> a >> b;
-    for(i=0; i<a.size(); i++)
-    {
-        a[i]=toupper (a[i]);
-        b[i]=toupper (b[i]);
-    }
-    if(a==b)
-    {
+
+
+using namespace std;
+
+int main()
+ {
+     string a;
+     string b;
+     int i;
+     cin >> a >> b;
+     for(i=0; i<a.size(); i++){
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+     }
+     if(a==b)
         cout << "0" << endl;
-    }
-    else
-    {
-        for (i=0; i<a.size(); i++)
-        {
-            if(a[i]>b[i])
-            {
+     else{
+        for(i=0;i<a.size();i++){
+            if(a[i]<b[i]){
                 cout << "-1" << endl;
-                 break;
+                break;
             }
-        }
-         for (i=0; i<a.size(); i++)
-        {
-            if(a[i]<b[i])
-            {
+            if(a[i]>b[i]){
                 cout << "1" << endl;
                 break;
             }
         }
-    }
-    return 0;
+     }
+
+     return 0;
 }
+
 
