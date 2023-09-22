110bb17c444955b5501186ee2f05419560b3baa9
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-09-23 01:04:13 +0600

    Create lab-12.cpp

---
+++ lab-12.cpp
@@ -0,0 +1,23 @@
+//Stones on the Table
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n ,a=0; cin >> n;
+    char c[n];
+    cin >> c;
+    for(int i=0; i<n; i++)
+
+
+        if(c[i]==c[i+1])
+            {
+                a++;
+            }
+
+
+     cout <<a;
+
+    return 0;
+}
