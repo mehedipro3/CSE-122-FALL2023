bcf582a82eb88ce3f8b4cb3a491ca4afb8c09271
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-10-06 12:29:39 +0600

    Elephant

---
+++ lab_05/Elephant.cpp
@@ -0,0 +1,22 @@
+// Elephant
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a;
+    cin >> a;
+    if(a%5==0)
+    {
+        cout << a/5 << endl;
+    }
+    else
+    {
+        cout << (a/5)+1 << endl;
+    }
+
+    return 0;
+}
